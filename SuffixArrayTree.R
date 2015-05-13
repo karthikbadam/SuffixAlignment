4f67098698f46dab9d6f088a65aeea93d58fb8de
karthikbadam <sbadam@purdue.edu>
2015-05-13 00:19:19 -0400

    suffix array and tree in R

--- SuffixArrayTree.R
+++ SuffixArrayTree.R
@@ -1,26 +1,40 @@
+
+
+# Art code
 # ./art_454 chr1.fa chr1_reads 2
 #./art_illumina -i chr1.fa -l 50 -f 5 -o chr1_reads_illumina
-require(BSgenome.Hsapiens.UCSC.hg19)
-seqChr = unmasked(Hsapiens$chr1)
-#sa <- sort(DNAStringSet(Views(seqChr1, start=seq_len(nchar(seqChr1)), length(seqChr1))))
 
-seqLength <- length(seqChr)
-sampleSize <- 100
-runs <- round(seqLength/sampleSize)
-matchSize <- 100
+# Suffix Array based alignment algorithm created by S. Karthik Badam
 
-library("ShortRead") 
+library(ShortRead) 
 library(rbenchmark)
 library(doParallel)
 registerDoParallel(cores = 4)
 library(foreach)
+library(gtools)
 
-# reads <- readFastq('chr1_reads.fq')
-reads <- readFastq('~/Documents/CMSC702/Project/chr1_reads.fq')
-rs <- sread(reads)
+# Reference genome
+require(BSgenome.Hsapiens.UCSC.hg19)
+seqChr = unmasked(Hsapiens$chr1)[100000:110000]
+seqLength <- length(seqChr)
 
+# Size of the suffix array loaded into memory
+sampleSize <- 1000
+runs <- round(seqLength/sampleSize)
+
+# Number of reads to be aligned
+matchSize <- 100
+
+# reads <- readFastq('~/Documents/CMSC702/Project/chr1_reads.fq')
+# Reads from the ART simulator
+reads <- readFastq('chr1_reads.fq')
+rs <- sort (sread(reads), decreasing=TRUE)
+
+# Looping through the entire suffix array --currently lets just do 1 segment of suffix array
 # for (i in 1:runs) {
 for (i in 1:1) {
+  
+  # Load the part of the suffix array for alignment
   if (i != runs) {
     sa <- DNAStringSet(Views(seqChr, start=seq(i*sampleSize, (i+1)*sampleSize-1), length(seqChr)))
     
@@ -29,65 +43,61 @@ for (i in 1:1) {
     
   }
   
+  # benchmark code -- comparing linear search, parallel linear search, and parallel binary search
   print(benchmark(replications = 1, order = "elapsed",
                   non_parallel = {
                     for (j in 1:matchSize) {
-                      foreach(ref = sa) %do% pmatch(rs[[j]], substr(ref, 1, length(rs[[j]]))) 
+                      foreach(ref = sa) %do% compareStrings(rs[[j]], substr(ref, 1, length(rs[[j]]))) 
                     }
                   }
-                  , parallel = {
-                    test <- foreach(ref = sa, .combine = cbind) %dopar%  for (j in 1:matchSize) {pmatch(rs[[j]], substr(ref, 1, length(rs[[j]])))}
-                    # pairwiseAlignment(rs[[j]], substr(ref, 1, length(rs[[j]])))
+                  , linear_parallel = {
+                    # Matching parallel
+                    test <- foreach(ref = sa, .combine = cbind) %dopar%  for (j in 1:matchSize) {
+                      compareStrings(rs[[j]], substr(ref, 1, length(rs[[j]])))
+                    }
+                  }
+                  , binary_parallel = {
+                   
+                    sa <- sort(sa)
+                    
+                    foreach(search = rs[1:10], .combine = cbind) %dopar%  {  
+                      
+                      # Binary search
+                      range <- c(1, length(sa))
+                      index <- 1
+                      while (range[2] > range[1]) {
+                        index <- as.integer(sum(range)/2)
+                        boolArray <- as.integer(search) < as.integer(substr(sa[[index]], 1, length(search)))
+                        if (!is.na(sum(boolArray))) {
+                          boolArray <- boolArray*1
+                          if (median(boolArray) == 1) {
+                            range[2] = index - 1
+                          } else if (median(boolArray) == 0) {
+                            range[1] = index + 1
+                          } else {
+                            break
+                          }
+                        } else {
+                          range[1] = index - 1
+                        }
+                      }
+                      compareStrings(search, substr(sa[[index]], 1, length(search)))
+                    }
                   }
   ))
 }
 
-for (i = 1; i <= runs; i++) {
-  if (i != runs) {
-    sa <- sort(DNAStringSet(Views(seqChr, start=seq(i*sampleSize, (i+1)*sampleSize), length(seqChr))))
-    
-  } else {
-    sa <- sort(DNAStringSet(Views(seqChr, start=seq(i*sampleSize, length(seqChr)), length(seqChr))))
-    
-  }
-  # do binary search
-  benchmark(replications = 1, order = "elapsed",
-            non_parallel = {
-              test1 <- for (j = 1 ; j <= length(rs); j++) {
-                for (k = 1; k <= length(sa); k++) {
-                  referencePrefix <- substr(sa[[k]], 1, length(rs[[j]]))
-                  pairwiseAlignment(rs[[j]], referencePrefix)
-                }
-              }
-            },
-            
-            parallel = {
-              test2 <- foreach(i = sa, .combine = cbind) %dopar% pairwiseAlignment(rs[[j]], substr(i, 1, length(rs[[j]])))
-            }  
-  )
-}
-
-
-# sa <- sort(DNAStringSet(Views(seqChr1, start=seq(1, 10000000), length(seqChr1))))
+# Suffix Tree created by S. Karthik Badam
 
 # Load fq file generated from art program
-
 library("ShortRead")
-# reads <- readFastq('chr1_reads.fq')
-reads <- readFastq('~/Documents/CMSC702/Project/chr1_reads.fq')
-rs <- sread(reads)
-# pairwiseAlignment(rs[[1]], rs[[1]])
-
-# library("ape")
-# reads <- read.dna("~/Documents/CMSC702/Project/chr1_reads_illumina.aln", format = "clustal")
-# sequences <- sread(reads)
-
-#pairWiseAlignment
-
-# library("Rsamtools")
-
 library("PST")
-testString <- toString(seqChr[100000:110000])
+
+reads <- readFastq('chr1_reads.fq')
+rs <- sread(reads)
+
+# Creating a suffix tree for a test string
+testString <- toString(seqChr[100000:101000])
 testString <- gsub("([A-Z])", "-\\1", testString)
 testString <- substr(testString, 2, nchar(testString))
 seq1 <- seqdef(testString)
